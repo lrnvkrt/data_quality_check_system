351601ae75f744c9321f57d8c14762fa4cf799f1
lrnvkrt <krist1na.larion0va@yandex.ru>
2025-04-10 18:36:28 +0300

    Initial commit

---
+++ src/main/java/dqcs/kafkaconsumerservice/KafkaConsumerServiceApplication.java
@@ -0,0 +1,11 @@
+package dqcs.kafkaconsumerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaConsumerServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaConsumerServiceApplication.class, args);
+    }
+}
