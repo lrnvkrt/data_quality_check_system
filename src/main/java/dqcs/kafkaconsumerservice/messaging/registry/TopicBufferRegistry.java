4d015acb1baa9e0ce2ef32eccedff2ef39ceeda2
lrnvkrt <krist1na.larion0va@yandex.ru>
2025-04-12 17:22:16 +0300

    Add Topic Registry

---
+++ src/main/java/dqcs/kafkaconsumerservice/messaging/registry/TopicBufferRegistry.java
@@ -0,0 +1,73 @@
+package dqcs.kafkaconsumerservice.messaging.registry;
+
+import dqcs.kafkaconsumerservice.config.kafka.properties.KafkaTopicProperties;
+import dqcs.kafkaconsumerservice.config.kafka.properties.TopicConfig;
+import dqcs.kafkaconsumerservice.exception.TopicNotConfiguredException;
+import dqcs.kafkaconsumerservice.messaging.dto.GenericEvent;
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.function.Consumer;
+
+@Component
+public class TopicBufferRegistry {
+
+    private final Logger logger = LoggerFactory.getLogger(TopicBufferRegistry.class);
+
+    private final KafkaTopicProperties kafkaTopicProperties;
+
+    private final Map<String, BlockingDeque<GenericEvent>> topicQueues = new ConcurrentHashMap<>();
+
+    @Autowired
+    public TopicBufferRegistry(KafkaTopicProperties kafkaTopicProperties) {
+        this.kafkaTopicProperties = kafkaTopicProperties;
+    }
+
+    @PostConstruct
+    public void init() {
+        for (TopicConfig config : kafkaTopicProperties.getTopics()) {
+            var queue = new LinkedBlockingDeque<GenericEvent>(config.capacity());
+            topicQueues.put(config.name(), queue);
+        }
+    }
+
+    public boolean offer(String topic, GenericEvent event, Consumer<List<GenericEvent>> onOverflowFlush) {
+        var queue = topicQueues.get(topic);
+        if (queue != null) {
+            if (!queue.offer(event)) {
+                List<GenericEvent> overflowBatch = new ArrayList<>();
+                queue.drainTo(overflowBatch);
+                logger.warn("Buffer for topic {} overflowed. Draining {} events", topic, overflowBatch.size());
+                onOverflowFlush.accept(overflowBatch);
+                return false;
+            }
+            return true;
+        } else {
+            throw new TopicNotConfiguredException(topic);
+        }
+    }
+
+
+    public BlockingQueue<GenericEvent> getQueue(String topic) {
+        return topicQueues.get(topic);
+    }
+
+    public Set<String> allTopics() {
+        return topicQueues.keySet();
+    }
+
+    public int getBatchSize(String topic) {
+        return kafkaTopicProperties.getTopics().stream().filter(t -> t.name().equals(topic)).findFirst().orElseThrow().capacity();
+    }
+}
